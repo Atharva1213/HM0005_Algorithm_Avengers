288ff77e77e270ef5f985279f949cd908fba6155
Atharva1213 <lendeatharva30@gmail.com>
2024-02-17 16:42:56 +0530

    adding the session to backed

--- server/app.js
+++ server/app.js
@@ -1,18 +1,31 @@
 //require the module
 const express=require("express"); 
 const dotenv=require("dotenv");
+const cors = require("cors");
+const sessions = require("express-session");
 const cookieParser = require("cookie-parser");
 dotenv.config({path:"./Config.env"}); 
 require("./db/database"); 
 const PORT=process.env.PORT;
+const SESSION_SECRET = process.env.SESSION_SECRET;
+const ONEWEEK = 1000 * 60 * 60 * 24 * 7;
 
 const app=express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+    sessions({
+      secret: SESSION_SECRET,
+      saveUninitialized: true,
+      cookie: { maxAge: ONEWEEK },
+      resave: false,
+    })
+  );
 
+app.use(cors())
+app.use(cookieParser());
 const loginRouter = require("./routes/login");
 const registerRouter = require("./routes/register"); 
-
-app.use(cookieParser());
-app.use(express.json());
 
 app.use("/api/login", loginRouter);
 app.use("/api/register", registerRouter);
--- server/routes/login.js
+++ server/routes/login.js
@@ -4,11 +4,11 @@ const User = require("../Modal/user_table");
 
 const router = express.Router();
 
+
 // API route for user login
 router.post("/", async (req, res) => {
   try {
     const { user_name, password } = req.body;
-
     const user = await User.findOne({ user_name });
 
     if (!user) {
@@ -21,7 +21,15 @@ router.post("/", async (req, res) => {
       return res.status(401).json({ error: "Invalid username or password" });
     }
 
-    res.json({message: "Login successful" });
+    req.session.user = {
+      userId: user._id,
+      userName: user.user_name,
+      userRole: user.user_data.user_role, 
+    };
+
+    console.log("Login request received from %s",req.session.user ? req.session.user.userName : "Unknown User");
+
+    res.json({ message: "Login successful", user: req.session.user });
   } catch (error) {
     console.error("Error in user login route:", error);
     res.status(500).json({ error: "Internal Server Error" });
